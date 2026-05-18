1066b7c790f6127f048ed9ba90b9d3cfccd2bf2c
sagar <sagardhavale84@gmail.com>
2026-05-18 14:40:20 +0530

    day 5 terraform

---
+++ main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = var.regin
+}
+
+# get ami latest #
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# create sg #
+resource "aws_security_group" "web_sg" {
+  name = "my-web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0weight"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-web-sg"
+  }
+}
+
+# ec2 instance #
+resource "aws_instance" "myserver" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
+    sudo yum install -y nodejs git
+    sudo npm install -g pm2
+  EOF
+
+  tags = {
+    Name = var.server_name
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,14 @@
+
+variable "regin" {
+ default =  "us-east-2"
+
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+
+variable "server_name" {
+  default = "cicd-server"
+}
